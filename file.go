a7e89e3e800a564dfdb7826ff1ee5326774ab4b6
sujit <s.baniya.np@gmail.com>
2026-01-14 20:50:21 +0545

    feat: Add options support for PreviewFolder function

--- file.go
+++ file.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/oarkflow/previewer/pkg/file"
+	"github.com/oarkflow/previewer/pkg/vfs"
 )
 
 func PreviewFile(filePath string) error {
@@ -18,6 +19,9 @@ func Preview(r io.Reader) error {
 	return file.Preview(r)
 }
 
-func PreviewFolder(folderPath string) error {
+func PreviewFolder(folderPath string, opts ...vfs.Options) error {
+	if len(opts) > 0 {
+		return file.PreviewFolderWithOptions(folderPath, opts[0])
+	}
 	return file.PreviewFolder(folderPath)
 }
